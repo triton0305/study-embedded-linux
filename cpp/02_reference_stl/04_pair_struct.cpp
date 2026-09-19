81371237ea335a0e9581519fe28afe7db499ee57
triton0305 <triton0305@gmail.copm>
2026-09-20 02:26:16 +0900

    TIL : lambda

--- cpp/02_reference_stl/04_pair_struct.cpp
+++ cpp/02_reference_stl/04_pair_struct.cpp
@@ -5,6 +5,7 @@
 #include <iomanip>
 #include <algorithm>
 
+
 struct Player
 {
   int hp;
@@ -17,8 +18,12 @@ int main()
 
   std::cout << point.first <<'\n';
   std::cout << point.second << '\n';
+  std::cout << '\n';
+
+//=============================================================================
 
   std::vector<Player> players;
+  bool descending = true;
 
   players.push_back({100, "knight"});
   players.push_back({80, "archer"});
@@ -29,15 +34,50 @@ int main()
     std::cout << std::left << std::setw(8) << player.name << " : " << player.hp << '\n';
   }
 
+  std::sort(players.begin(), players.end(),
+            [descending](const Player& a, const Player& b)
+            {
+              return descending ? a.hp > b.hp : a.hp < b.hp;
+            });
+
+  for(const Player& player : players)
+  {
+    std::cout << std::left << std::setw(8) << player.name << " : " << player.hp << '\n';
+  }
+
+
+  std::cout << '\n';
+  
+//==================================================================
+
   std::vector<int> nums = {50, 20, 30, 10, 40};
   std::sort(nums.begin() + 2, nums.end());
-  // end() one past the last element
+  // end() is one past the last element
 
-  for(auto it = nums.begin() ; it != nums.end() ; it++)
+  for(auto it = nums.begin() ; it != nums.end() ; ++it)
   {
     std::cout << *it << '\n';
   }
   
+
+
+  //======================================================
+  bool enabled = true;
+  
+  auto valueCapture = [enabled]()
+  {
+    std::cout << "valueCapture : " << enabled << '\n';
+  };
+
+  auto referenceCapture = [&enabled]()
+  {
+    std::cout <<"referenceCapture : " << enabled << '\n';
+  };
+
+  enabled = false;
+
+  valueCapture();
+  referenceCapture();
 
   return 0;
 }
